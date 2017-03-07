a601b861c3dd5f379fea089919a67c39dc5ae7d6
sAbhay <abhayaaditsinghal@gmail.com>
2017-03-07 13:09:38 +0530

    Change #1
    Made each sort into its own function
    Added IdkSort()
    Named own sort to SinghalSort() - naming by Zack

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -12,25 +12,85 @@ void ofApp::setup(){
 //--------------------------------------------------------------
 void ofApp::update(){
     
+    SinghalSort();
     
-   /*if(cycleCount <= length) // bubble sort
+    }
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+    ofBackground(0);
+    ofSetColor(255);
+    
+    for(int i = 0; i < length; i++)
     {
-       for(int i = 0; i < length-1-cycleCount; i++)
+        ofDrawRectangle((ofGetWidth()/length)*i, ofGetHeight() - n[i], ofGetWidth()/length, n[i]);
+    }
+}
+
+void ofApp::BubbleSort()
+{
+    if(cycleCount <= length) // bubble sort
+    {
+        for(int i = 0; i < length-1-cycleCount; i++)
         {
             if(n[i] > n[i+1])
             {
                 int temp = n[i];
-            
+                
                 n[i] = n[i+1];
                 n[i+1] = temp;
             }
         }
     }
     
-    cycleCount++;*/
-    
-    for(int i = 1; i < length-1-cycleCount; i++) //original sort
+    cycleCount++;
+}
+
+void ofApp::CocktailSort()
+{
+    //for(int i = 0; i < length/2; i++) //cocktail shaker sort
     {
+        //        bool swapped = false;
+        
+        for(int j = length - cycleCount; j > cycleCount; j--)
+        {
+            if(n[j] > n[j-1])
+            {
+                int temp = n[j];
+                
+                n[j] = n[j-1];
+                n[j-1] = temp;
+                
+//              swapped = true;
+            }
+        }
+        
+        for(int j = cycleCount; j < length - cycleCount; j++)
+        {
+            if(n[j] < n[j+1])
+            {
+                int temp = n[j];
+                
+                n[j] = n[j+1];
+                n[j+1] = temp;
+                
+//              swapped = true;
+            }
+        }
+        
+        cycleCount++;
+        
+        // if(!swapped) break;
+    }
+}
+
+void ofApp::SinghalSort()
+{
+    for(int i = 1; i < length-1-cycleCount; i++) //Singhal sort
+    {
+        ofSetColor(255);
+    
         if(n[i] > n[i+1])
         {
             int temp = n[i];
@@ -46,53 +106,37 @@ void ofApp::update(){
             n[i] = n[i-1];
             n[i-1] = temp;
         }
+        
+        ofDrawRectangle((ofGetWidth()/length)*i, ofGetHeight() - n[i], ofGetWidth()/length, n[i]);
     }
     
     cycleCount++;
-    
-    /*//for(int i = 0; i < length/2; i++) //cocktail shaker sort
-    {
-//        bool swapped = false;
-    
-        for(int j = length - cycleCount - 2; j > cycleCount; j--)
-        {
-            if(n[j] > n[j-1])
-            {
-                int temp = n[j];
-                
-                n[j] = n[j-1];
-                n[j-1] = temp;
-                
-//                swapped = true;
-            }
-        }
-    
-        for(int j = cycleCount; j < length - cycleCount - 1; j++)
-        {
-            if(n[j] < n[j+1])
-            {
-                int temp = n[j];
-                
-                n[j] = n[j+1];
-                n[j+1] = temp;
-                
-//                swapped = true;
-            }
-        }
-        
-        cycleCount++;
-        
-       // if(!swapped) break;
-    }*/
 }
 
-//--------------------------------------------------------------
-void ofApp::draw(){
-    ofBackground(0);
-    ofSetColor(255);
-    
-    for(int i = 0; i < length; i++)
+void ofApp::IdkSort()
+{
+    for(int i = 0; i < length/mergeCycles; i+=mergeCycles)
     {
-        ofDrawRectangle((ofGetWidth()/length)*i, ofGetHeight() - n[i], ofGetWidth()/length, n[i]);
+        for(int j = 0; j < length/mergeCycles; j+=mergeCycles)
+        {
+    
+            if(i != j)
+            {
+                if(n[i] < n[j])
+                {
+                    int temp = n[i];
+                    
+                    n[i] = n[j];
+                    n[j] = temp;
+                }
+            }
+        }
     }
+    
+    mergeCycles++;
+}
+
+void ofApp::MergeSort()
+{
+    
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -9,8 +9,15 @@ class ofApp : public ofBaseApp{
 		void update();
 		void draw();
     
+        void BubbleSort();
+        void CocktailSort();
+        void SinghalSort();
+        void IdkSort();
+        void MergeSort();
+    
     private:
         int n[1440];
         int length = 1440;
         int cycleCount = 0;
+        int mergeCycles = 1;
 };
